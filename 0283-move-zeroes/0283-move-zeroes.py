15b340f8fe475d942b829d79a3421c49450e6408
Hari Om Upadhyay <143203166+omupadhyay304@users.noreply.github.com>
2024-10-12 10:41:43 +0530

    Time: 114 ms (78.65%), Space: 12.7 MB (95.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,7 +7,7 @@ class Solution(object):
         
         l = 0
         for r in range(len(nums)):
-            if nums[r]:
+            if nums[r] != 0:
                 nums[l] , nums[r] = nums[r], nums[l]
                 l += 1
         return nums
